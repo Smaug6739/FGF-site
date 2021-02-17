fcce1c25389070889aea90ad825e29f8c2a194dc
Raphael <veauville.raphael@gmail.com>
2021-02-17 20:41:26 +0100

    Update backend

--- backend/controllers/requests.js
+++ backend/controllers/requests.js
@@ -42,12 +42,29 @@ exports.postGeneral = (req, res) =>{
     .catch(error => res.json(checkAndChange(new Error(error))))
 }
 exports.postJob = (req, res) =>{
-    Request.postJob(req.body.q1,req.body.q2,req.body.q3,req.body.q4,req.body.q5,req.body.q6,req.params.userId)
+    Request.postJob(
+        req.body.q1,
+        req.body.q2,
+        req.body.q3,
+        req.body.q4,
+        req.body.q5,
+        req.body.q6,
+        req.params.userId)
     .then(result => res.json(checkAndChange(result)))
     .catch(error => res.json(checkAndChange(new Error(error))))
 }
 exports.postPartner = (req, res) =>{
-    Request.postPartner(req.body.pseudo,req.body.email,req.body.age,req.body.q1,req.body.q2,req.body.q3,req.body.q4,req.body.q4,req.body.q5,req.body.q6,req.params.userId)
+    Request.postPartner(
+        req.body.pseudo,
+        req.body.email,
+        req.body.age,
+        req.body.q1,
+        req.body.q2,
+        req.body.q3,
+        req.body.q4,
+        req.body.q5,
+        req.body.q6,
+        req.params.userId)
     .then(result => res.json(checkAndChange(result)))
     .catch(error => res.json(checkAndChange(new Error(error))))
 }
