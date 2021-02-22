d36f144f21b89e5c25ac3adb2923353d114cd50c
Raphael <veauville.raphael@gmail.com>
2021-02-22 10:16:27 +0100

    Deploy options

---
+++ ecosystem.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  apps : [{
+    name        : "fgf-server",
+    script      : "./backend/server.js",
+    instances  : 2,
+    exec_mode  : "cluster",
+    env: {
+      "NODE_ENV": "development",
+    },
+    env_production : {
+       "NODE_ENV": "production"
+    }
+  },{
+    name       : "fgf-frontend",
+    script     : "./__frontend/app.js",
+    //watch       : true,
+    //ignore_watch: ['uploads'],
+    instances  : 2,
+    exec_mode  : "cluster",
+   /*error_file: './logs',
+    out_file: './logs',
+    log_file: './logs',
+    time : true,*/
+    env: {
+      "NODE_ENV": "development",
+    },
+    env_production : {
+       "NODE_ENV": "production"
+    }
+  }]
+
+  /*deploy : {
+    production : {
+      user : 'fgf',
+      host : '161.97.66.77',
+      ref  : 'origin/master',
+      repo : 'https://github.com/SmaugDev/FGF-Site',
+      path : '/home/fgf-site/prod',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }*/
+};
