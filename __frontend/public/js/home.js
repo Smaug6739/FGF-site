8fac5751f86f1ffd85bb4b7a4dd1dfacd3faca9b
Raphael <veauville.raphael@gmail.com>
2021-03-06 14:03:18 +0100

    Update slider

--- __frontend/public/js/home.js
+++ __frontend/public/js/home.js
@@ -24,5 +24,17 @@ function showSlides(n) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  if(dots[slideIndex-1]) dots[slideIndex-1].className += " active";
 }
+/*showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+}*/
