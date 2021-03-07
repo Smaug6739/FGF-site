9d8f7093c294148ca373624bfd1f09d5909a96dc
Raphael <veauville.raphael@gmail.com>
2021-03-07 13:12:19 +0100

    Update

--- __frontend/public/js/album/album.js
+++ __frontend/public/js/album/album.js
@@ -5,4 +5,29 @@ window.onload = function(){
     const pagePrecedente = parseInt(page) - 1
     document.getElementById("suivant").setAttribute("href", `/album/${pageSuivante}`)
     document.getElementById("precedent").setAttribute("href", `/album/${pagePrecedente}`)
+
+    /* ALBUM */
+    var modal = document.getElementById("myModal");
+
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    var imgs = document.getElementsByClassName("image")
+    const imgsArray = Array.from(imgs)
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+    imgsArray.forEach(function(img) {
+    img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+    }
+    });
+
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+    modal.style.display = "none";
+    }
 }
