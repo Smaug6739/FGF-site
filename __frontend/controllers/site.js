486de76a0f0ac4f319b457c7b8f8a20cd32c9c33
Raphael <veauville.raphael@gmail.com>
2021-03-18 14:35:51 +0100

    Fix 404 infos page

--- __frontend/controllers/site.js
+++ __frontend/controllers/site.js
@@ -1,55 +1,60 @@
 const path = require('path');
-const {statusUser} = require('../functions');
+const { statusUser } = require('../functions');
 const axios = require('axios')
 const config = require('../config.js')
 
-exports.getAnnouncements = (req,res) => {
+exports.getAnnouncements = (req, res) => {
     axios.get(`http://localhost:8080/api/v1/announcements/all/${req.params.page}`)
-    .then(async(responce) => {
-        if(responce.data.status === 'success'){
-            res.render(path.join(__dirname, '../pages/announcements/lasts.ejs'),{
-                userConnected : await statusUser(req.session),
-                announcements : responce.data.result,
-            })
-        }else{
-            res.render(path.join(__dirname, '../pages/error.ejs'),{
-                userConnected : await statusUser(req.session),
-                error : responce.data.message,
-            })
-        }
-    })
-    .catch(async(error) => {
-        res.render(path.join(__dirname, '../pages/error.ejs'),{
-            userConnected : await statusUser(req.session),
-            error : error,
+        .then(async (responce) => {
+            if (responce.data.status === 'success') {
+                res.render(path.join(__dirname, '../pages/announcements/lasts.ejs'), {
+                    userConnected: await statusUser(req.session),
+                    announcements: responce.data.result,
+                })
+            } else {
+                res.render(path.join(__dirname, '../pages/error.ejs'), {
+                    userConnected: await statusUser(req.session),
+                    error: responce.data.message,
+                })
+            }
         })
-    });
+        .catch(async (error) => {
+            res.render(path.join(__dirname, '../pages/error.ejs'), {
+                userConnected: await statusUser(req.session),
+                error: error,
+            })
+        });
 }
 
 exports.getHome = async (req, res) => {
     const articles = await axios.get(`http://localhost:8080/api/v1/articles/all/1`)
     const annonces = await axios.get(`http://localhost:8080/api/v1/announcements/all/1`)
-    const album =    await axios.get(`http://localhost:8080/api/v1/album/valides/1`)
-    res.render(path.join(__dirname, '../pages/index.ejs'),{
-        userConnected : await statusUser(req.session),
-        articles : articles.data.result,
-        annonces : annonces.data.result,
+    const album = await axios.get(`http://localhost:8080/api/v1/album/valides/1`)
+    res.render(path.join(__dirname, '../pages/index.ejs'), {
+        userConnected: await statusUser(req.session),
+        articles: articles.data.result,
+        annonces: annonces.data.result,
         albums: album.data.result
     })
-        
+
 }
 
+exports.getInfos = async (req, res) => {
+    res.render(path.join(__dirname, '../pages/infos.ejs'), {
+        userConnected: await statusUser(req.session),
+    })
+}
 exports.getTerms = async (req, res) => {
-    res.render(path.join(__dirname, '/pages/terms.ejs'),{
-        userConnected : await statusUser(req.session),
+    res.render(path.join(__dirname, '../pages/terms.ejs'), {
+        userConnected: await statusUser(req.session),
     })
 }
 
 exports.getPrivacy = async (req, res) => {
-    res.render(path.join(__dirname, '/pages/privacy.ejs'),{
-        userConnected : await statusUser(req.session),
+    res.render(path.join(__dirname, '../pages/privacy.ejs'), {
+        userConnected: await statusUser(req.session),
     })
 }
 exports.getAPI = async (req, res) => {
-    res.status(200).send({api:config.urlAPI})
+    res.status(200).send({ api: config.urlAPI })
 }
--- __frontend/routes/site.js
+++ __frontend/routes/site.js
@@ -2,9 +2,10 @@ var express = require('express')
 let SiteRouter = express.Router()
 const siteCtrl = require('../controllers/site');
 
-SiteRouter.get('/',siteCtrl.getHome)
-SiteRouter.get('/terms',siteCtrl.getTerms)
-SiteRouter.get('/privacy',siteCtrl.getPrivacy)
+SiteRouter.get('/', siteCtrl.getHome)
+SiteRouter.get('/infos', siteCtrl.getInfos)
+SiteRouter.get('/terms', siteCtrl.getTerms)
+SiteRouter.get('/privacy', siteCtrl.getPrivacy)
 SiteRouter.get('/announcements/:page', siteCtrl.getAnnouncements)
 SiteRouter.get('/config-api', siteCtrl.getAPI)
 
