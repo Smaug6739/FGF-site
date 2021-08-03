7de1a6bee1a0b28832a9ef75d13a4fe26f95698d
Raphael <veauville.raphael@gmail.com>
2021-08-03 15:44:29 +0200

    Update username

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,31 +1,31 @@
 module.exports = {
-  apps : [{
-    name        : "fgf-server",
-    script      : "./backend/server.js",
-    instances  : 2,
-    exec_mode  : "cluster",
+  apps: [{
+    name: "fgf-server",
+    script: "./backend/server.js",
+    instances: 2,
+    exec_mode: "cluster",
     env: {
       "NODE_ENV": "development",
     },
-    env_production : {
-       "NODE_ENV": "production"
+    env_production: {
+      "NODE_ENV": "production"
     }
-  },{
-    name       : "fgf-frontend",
-    script     : "./__frontend/app.js",
+  }, {
+    name: "fgf-frontend",
+    script: "./__frontend/app.js",
     //watch       : true,
     //ignore_watch: ['uploads'],
-    instances  : 2,
-    exec_mode  : "cluster",
-   /*error_file: './logs',
-    out_file: './logs',
-    log_file: './logs',
-    time : true,*/
+    instances: 2,
+    exec_mode: "cluster",
+    /*error_file: './logs',
+     out_file: './logs',
+     log_file: './logs',
+     time : true,*/
     env: {
       "NODE_ENV": "development",
     },
-    env_production : {
-       "NODE_ENV": "production"
+    env_production: {
+      "NODE_ENV": "production"
     }
   }]
 
@@ -34,7 +34,7 @@ module.exports = {
       user : 'fgf',
       host : '161.97.66.77',
       ref  : 'origin/master',
-      repo : 'https://github.com/SmaugDev/FGF-Site',
+      repo : 'https://github.com/Smaug6739/FGF-Site',
       path : '/home/fgf-site/prod',
       'pre-deploy-local': '',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
